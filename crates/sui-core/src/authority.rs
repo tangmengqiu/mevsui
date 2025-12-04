56072680b3e21a029a49ef4319c881df104dc2c4
唐孟秋 <mqtang@tangmengqius-Mac-mini.local>
2025-12-03 21:13:22 -0500

    fix sui lock status

--- crates/sui-core/src/authority.rs
+++ crates/sui-core/src/authority.rs
@@ -94,7 +94,7 @@ use tracing::{debug, error, info, instrument, warn};
 
 use self::authority_store::ExecutionLockWriteGuard;
 use self::authority_store_pruner::{AuthorityStorePruningMetrics, PrunerWatermarks};
-pub use authority_store::{AuthorityStore, ResolverWrapper, UpdateType};
+pub use authority_store::{AuthorityStore, ResolverWrapper, UpdateType, SuiLockResult,ObjectLockStatus};
 use mysten_metrics::{monitored_scope, spawn_monitored_task};
 
 use crate::jsonrpc_index::IndexStore;
